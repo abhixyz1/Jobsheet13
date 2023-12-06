45e0c7307d18297ac4d4353ba21e7c0f0a5f1e33
abhixyz1 <irfanhendra90@gmail.com>
2023-12-06 11:44:10 +0700

    Percobaan 1

---
+++ TerimaKasih_20.java
@@ -0,0 +1,9 @@
+public class TerimaKasih_20 {
+    public static void UcapanterimaKasih(){
+        System.out.println("Thank you for being the best teacher in the world.\n"
+        + "You inspired in me a love for learning and made me feel like I could ask you anything.");
+    }
+    public static void main(String[] args) {
+        UcapanterimaKasih();
+    }
+}
