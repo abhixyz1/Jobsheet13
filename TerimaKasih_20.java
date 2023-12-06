e1172d92e248e06eafd07bbd99ea129afc99e7f1
abhixyz1 <irfanhendra90@gmail.com>
2023-12-06 13:02:18 +0700

    Percobaan bioskop dengan scanner dan modif semua

--- TerimaKasih_20.java
+++ TerimaKasih_20.java
@@ -1,9 +1,16 @@
 public class TerimaKasih_20 {
-    public static void UcapanterimaKasih(){
-        System.out.println("Thank you for being the best teacher in the world.\n"
+    // public static void UcapanterimaKasih() {
+    // System.out.println("Thank you for being the best teacher in the world.\n"
+    // + "You inspired in me a love for learning and made me feel like I could ask
+    // you anything.");
+    // }
+
+    // public static void main(String[] args) {
+    // UcapanterimaKasih();
+    // }
+    public static void main(String[] args) {
+        System.out.println("Thank you for being the best teacher in the world.\n" 
         + "You inspired in me a love for learning and made me feel like I could ask you anything.");
     }
-    public static void main(String[] args) {
-        UcapanterimaKasih();
-    }
+
 }
