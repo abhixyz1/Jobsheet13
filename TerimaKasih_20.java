0cf0137398e15b01cce5dc817deb3cbaf4c22011
abhixyz1 <irfanhendra90@gmail.com>
2023-12-06 17:12:44 +0700

    percobaan 1 dan 2

--- TerimaKasih_20.java
+++ TerimaKasih_20.java
@@ -1,18 +1,22 @@
 public class TerimaKasih_20 {
-    // public static void UcapanterimaKasih() {
-    // System.out.println("Thank you for being the best teacher in the world.\n"
-    // + "You inspired in me a love for learning and made me feel like I could ask
-    // you anything.");
-    // }
+    public static void UcapanterimaKasih() {
+    System.out.println("Thank you for being the best teacher in the world.\n" 
+    + "You inspired in me a love for learning and made me feel like I could ask you anything.");
+    }
+    public static void UcapanTambahan(String ucapan){
+        System.out.println(ucapan);
+    }
 
-    // public static void main(String[] args) {
-    // UcapanterimaKasih();
-    // }
+    public static void main(String[] args) {
+    UcapanterimaKasih();
+    String ucapan = "Terima Kasih Pak.. Bu.. Semoga Sehat Selalu";
+    UcapanTambahan(ucapan);
+    }
 
     
-    public static void main(String[] args) {
-        System.out.println("Thank you for being the best teacher in the world.\n" 
-        + "You inspired in me a love for learning and made me feel like I could ask you anything.");
-    }
+    // public static void main(String[] args) {
+    //     System.out.println("Thank you for being the best teacher in the world.\n" 
+    //     + "You inspired in me a love for learning and made me feel like I could ask you anything.");
+    // }
 
 }
