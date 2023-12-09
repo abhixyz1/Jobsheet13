2eba14c69107c8ce9f64edc71c9fcb6377cb4e8c
abhixyz1 <irfanhendra90@gmail.com>
2023-12-09 23:00:56 +0700

    percobaan 4 + modif

--- UcapanTerimaKasih_20.java
+++ UcapanTerimaKasih_20.java
@@ -5,16 +5,23 @@ public class UcapanTerimaKasih_20 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Tuliskan NAMA orang yang ingin anda beri ucapan: ");
         String namaOrang = sc.nextLine();
-        sc.close();
         return namaOrang;
     }
     public static void UcapanTerimaKasih(){
         String nama = PenerimaUcapan();
+        String ucaptambah = UcapanTambahan();
         System.out.println("Thank you " + nama +" for being the best teacher in the world.\n"+
         "You inspired in me a love for learning and made me feel like I could ask you anything");
+        System.out.println("Ucapan Tambahan untuk " + nama + " adalah " +ucaptambah);
     }
     public static void main(String[] args) {
         UcapanTerimaKasih();
     }
-
+    public static String UcapanTambahan(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Tuliskan ucapan tambahan yang ingin di berikan: ");
+        String ucapantambahan = sc.nextLine();
+        sc.close();
+        return ucapantambahan;
+    }
 }
